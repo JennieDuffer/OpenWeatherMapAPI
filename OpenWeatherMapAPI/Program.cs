45de479764a326de929fb1a247357dbde414d53d
Jennie Duffer <jenniej297@gmail.com>
2022-09-12 13:04:11 -0500

    initial commit

--- OpenWeatherMapAPI/Program.cs
+++ OpenWeatherMapAPI/Program.cs
@@ -1,11 +1,13 @@
-﻿using Newtonsoft.Json.Linq;
+﻿
+using Newtonsoft.Json.Linq;
 
 Console.WriteLine("Please enter the city you wish to see the weather for: ");
 var cityName = Console.ReadLine();
 
 var client = new HttpClient();
+var apiKey = "6de509898672060e0c90399fa3cdf525";
 
-var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid=6de509898672060e0c90399fa3cdf525&units=imperial";
+var weatherURL = $"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=imperial";
 var weatherResponse = client.GetStringAsync(weatherURL).Result;
 var weatherObject = JObject.Parse(weatherResponse);
 Console.WriteLine($" Temperature: {weatherObject["main"]["temp"]}");
